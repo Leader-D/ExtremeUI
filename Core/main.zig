c1f6ef1e5f9519d5f74b72082206a064d08859d4
Loukmane Hadj Said <loukmanehadjsaid56@gmail.com>
2026-04-03 07:53:13 +0100

    Import Platform and call open in main function
    Added platform import and call to open platform.

--- Core/main.zig
+++ Core/main.zig
@@ -1,12 +1,12 @@
-const std     = @import("std");
-const builtin = @import("builtin");
-const Config  = @import("Config.zig");
-const Engine  = @import("Shad-gines/SPIR-V/Runtime/Engine.zig");
+const std      = @import("std");
+const builtin  = @import("builtin");
+const Config   = @import("Config.zig");
+const Engine   = @import("Shad-gines/SPIR-V/Runtime/Engine.zig");
+const Platform = @import("Platform/Linux_win.zig");
 
 pub fn main() !void {
     std.debug.print("ExtremeUI v0.0.1 starting...\n", .{});
     std.debug.print("Platform: {s}\n", .{@tagName(builtin.os.tag)});
 
-    Config.init(.{ .width = 1920, .height = 1080, .title = "ExtremeUI App" });
-
+    try Platform.open();
 }
